18a90b34513bf7abf7a6f20f37e4c2160a321351
John Malapit <johncmalapit@gmail.com>
2022-08-29 23:19:07 -0400

    add focus component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import ClockComponent from "./components/ClockComponent";
 import Greeting from "./components/Greeting";
 import "./App.css";
+import FocusComponent from "./components/FocusComponent";
 
 function App() {
   return (
     <div className="App">
       <ClockComponent />
       <Greeting />
+      <FocusComponent />
     </div>
   );
 }
---
+++ frontend/src/components/FocusComponent.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import "./FocusComponent.css";
+
+const FocusComponent = () => {
+  const [formFocus, setFormFocus] = useState("");
+  const [focus, setFocus] = useState(null);
+  const localFocus = localStorage.getItem("focus");
+
+  useEffect(() => {
+    if (localFocus) {
+      setFocus(localFocus);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit");
+    localStorage.setItem("focus", formFocus);
+    setFocus(formFocus);
+  };
+
+  return (
+    <div className="focus-component">
+      {!focus && (
+        <>
+          <h3>What is your main focus for today?</h3>
+          <form action="#" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="focus-input"
+              id="focus-input"
+              value={formFocus}
+              onChange={(e) => setFormFocus(e.target.value)}
+            />
+          </form>
+        </>
+      )}
+      {focus && <>[] {focus}</>}
+    </div>
+  );
+};
+
+export default FocusComponent;
