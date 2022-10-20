996647ca3507eb36c544f0ffe49005d1ac37a603
John Malapit <johncmalapit@gmail.com>
2022-10-20 16:20:44 -0400

    revise app

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ mongoose
 
 app.use(morgan('dev'));
 app.use(cors());
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static('client/build'));
 app.use(express.json());
 app.use(middleware.userExtractor);
 
@@ -37,7 +37,7 @@ app.use('/api/login', loginRouter);
 
 // UNHANDLED
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 module.exports = app;
