c42f07f79601e3dea457e1005d6ebc46d5ac4ba9
John Malapit <johncmalapit@gmail.com>
2022-10-20 14:37:51 -0400

    add unhandled path

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
-const config = require("./utils/config");
-const express = require("express");
+const config = require('./utils/config');
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const logger = require("./utils/logger");
-const mongoose = require("mongoose");
-const middleware = require("./utils/middleware");
-const morgan = require("morgan");
+const cors = require('cors');
+const logger = require('./utils/logger');
+const mongoose = require('mongoose');
+const middleware = require('./utils/middleware');
+const morgan = require('morgan');
 
-const usersRouter = require("./controllers/users");
-const wallpapersRouter = require("./controllers/wallpapers");
-const weatherRouter = require("./controllers/weather");
-const loginRouter = require("./controllers/login");
+const usersRouter = require('./controllers/users');
+const wallpapersRouter = require('./controllers/wallpapers');
+const weatherRouter = require('./controllers/weather');
+const loginRouter = require('./controllers/login');
 
-logger.info("connecting to", config.MONGODB_URI);
+logger.info('connecting to', config.MONGODB_URI);
 
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
-    logger.info("connected to MongoDB");
+    logger.info('connected to MongoDB');
   })
   .catch((err) => {
-    logger.error("error connecting to MongoDB: ", err.message);
+    logger.error('error connecting to MongoDB: ', err.message);
   });
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(cors());
-app.use(express.static("build"));
+app.use(express.static('build'));
 app.use(express.json());
 app.use(middleware.userExtractor);
 
-app.use("/api/users", usersRouter);
-app.use("/api/wallpapers", wallpapersRouter);
-app.use("/api/weather", weatherRouter);
-app.use("/api/login", loginRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/wallpapers', wallpapersRouter);
+app.use('/api/weather', weatherRouter);
+app.use('/api/login', loginRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 module.exports = app;
