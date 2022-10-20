a47f74956206ff4fab989538f78b57be5045b68d
John Malapit <johncmalapit@gmail.com>
2022-10-20 13:14:38 -0400

    add react-router-dom

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
@@ -13,7 +15,14 @@ root.render(
     <ContextPomo>
       <ContextAuth>
         <ContextUI>
-          <App />
+          <Router>
+            <Routes>
+              <Route
+                path='/'
+                element={<App />}
+              />
+            </Routes>
+          </Router>
         </ContextUI>
       </ContextAuth>
     </ContextPomo>
