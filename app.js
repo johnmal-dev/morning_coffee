14b379e7576ab5c5a64022e6ca010155dfcb4383
John Malapit <johncmalapit@gmail.com>
2022-10-22 17:45:26 -0400

    refactor static line

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ mongoose
 
 app.use(morgan('dev'));
 app.use(cors());
-app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(express.json());
 app.use(middleware.userExtractor);
 
